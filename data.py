ab6251901b1c7d2c3676356df664549af08b1c7d
Anteneh Mulu <anteneh.mulu1921@gmail.com>
2025-01-16 21:32:09 +0300

    Quiz Project Phase 1

--- data.py
+++ data.py
@@ -1,14 +1,92 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+            {
+
+                "question": "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
+                "correct_answer": "True",
+                
+            },
+            {
+                
+                "question": "&quot;I disapprove of what you say, but I will defend to the death your right to say it&quot; is a quote from French philosopher Voltaire.",
+                "correct_answer": "False",
+               
+            },
+            {
+                
+                "question": "Franz Joseph I was the last emperor of Austria.",
+                "correct_answer": "False",
+               
+            },
+            {
+                
+                "question": "The Hundred Years&#039; War was fought for more than a hundred years.",
+                "correct_answer": "True",
+                
+            },
+            {
+                
+                "question": "The M41 Walker Bulldog remains in service in some countries to this day.",
+                "correct_answer": "True",
+                
+            },
+            {
+                
+                "question": "Abraham Lincoln was the first U.S. President to be born outside the borders of the thirteen original states. ",
+                "correct_answer": "True",
+                
+            },
+            {
+                
+                "question": "The Korean War ended in 1953 without any ceasefire.",
+                "correct_answer": "False",
+               
+            },
+            {
+                
+                "question": "The first televised presidential debate was between Jimmy Carter and Gerald Ford.",
+                "correct_answer": "False",
+               
+            },
+            {
+                
+                "question": "Brezhnev was the 5th leader of the USSR.",
+                "correct_answer": "True",
+                
+            },
+            {
+                
+                "question": "If you grab the bladed end of a longsword in a specific way, you will not cut yourself.",
+                "correct_answer": "True",
+                
+            },
+            {
+                
+                "question": "Adolf Hitler was accepted into the Vienna Academy of Fine Arts.",
+                "correct_answer": "False",
+               
+            },
+            {
+                
+                "question": "United States President Ronald Reagan was the first president to appoint a woman to the Supreme Court. ",
+                "correct_answer": "True",
+                
+            },
+            {
+                
+                "question": "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.",
+                "correct_answer": "True",
+                
+            },
+            {
+                
+                "question": "In World War II, Hawker Typhoons served in the Pacific theater.",
+                "correct_answer": "False",
+               
+            },
+            {
+                
+                "question": "Assyrian king Sennacherib&#039;s destruction of Babylon in 689 BCE was viewed as a triumph by other Assyrian citizens.",
+                "correct_answer": "False",
+               
+            }
 ]
---
+++ main.py
@@ -0,0 +1,17 @@
+from data import question_data
+from quiz_brain import QuizBrain
+
+class Question:
+   def __init__(self,text,answer):
+       self.text=text
+       self.answer=answer
+question_bank=[]
+for i in range(0,len(question_data)):
+    question=Question(question_data[i]['question'],question_data[i]['correct_answer'])
+    question_bank.append(question)
+
+quiz=QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+print("You Have completed the Quiz")
+print(f"Your Final Score Was: {quiz.score}/{quiz.question_number}")
